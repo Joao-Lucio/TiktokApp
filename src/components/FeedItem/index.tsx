3576ee18839d3b5fa2f5a206e7e932f5884e09b1
JOAO-LUCIO <joao.silvarrr@gmail.com>
2026-07-27 12:02:36 -0400

    feat: implement video feed using react-native-video with scroll-based playback control

---
+++ src/components/FeedItem/index.tsx
@@ -0,0 +1,168 @@
+import { useEffect, useState } from 'react';
+import {
+  Text,
+  StyleSheet,
+  Pressable,
+  Dimensions,
+  View,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import Video, { ResizeMode } from 'react-native-video';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const { height: heightScreen } = Dimensions.get('window');
+
+interface FeedItemProps {
+  data: {
+    id: string;
+    url: string;
+    name: string;
+    description: string;
+    likes: number;
+    comments: number;
+  };
+  currentVisibleItem?: {
+    id: string;
+    url: string;
+    name: string;
+    description: string;
+    likes: number;
+    comments: number;
+  };
+}
+
+export function FeedItem({ data, currentVisibleItem }: FeedItemProps) {
+  const [paused, setPaused] = useState(false);
+  const [liked, setLiked] = useState(false);
+
+  function togglePlayback() {
+    setPaused(currentPaused => !currentPaused);
+  }
+
+  function likeButton() {
+    setLiked(isLiked => !isLiked);
+  }
+
+  function formatNumber(number: number): string {
+    if (number >= 1000) {
+      return `${number / 1000}k`;
+    }
+    return number.toString();
+  }
+
+  useEffect(() => {
+    if (currentVisibleItem?.id === data.id) {
+      setPaused(false);
+      return;
+    }
+    setPaused(true);
+  }, [currentVisibleItem]);
+
+  return (
+    <View>
+      <View style={[styles.info, { bottom: 100 }]}>
+        <Text style={styles.name}>{data.name}</Text>
+        <Text numberOfLines={2} style={styles.description}>
+          {data.description}
+        </Text>
+      </View>
+      <View style={styles.actions}>
+        <TouchableOpacity onPress={likeButton}>
+          <Ionicons name="heart" size={35} color={liked ? '#DC143C' : '#FFF'} />
+          <Text style={styles.actionText}>{formatNumber(data.likes)}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Ionicons name="chatbubble-ellipses" size={35} color="#FFF" />
+          <Text style={styles.actionText}>{formatNumber(data.comments)}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Ionicons name="bookmark" size={35} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+      <Video
+        source={{ uri: data.url }}
+        style={{ width: '100%', height: heightScreen }}
+        pointerEvents="none"
+        resizeMode={ResizeMode.CONTAIN}
+        paused={paused}
+        repeat
+        muted={false}
+        onLoad={() => {
+          console.log('Vídeo carregado');
+        }}
+        onLoadStart={() => {
+          console.log('Carregando vídeo');
+        }}
+        onBuffer={({ isBuffering }) => {
+          console.log('Buffer:', isBuffering);
+        }}
+        onError={error => {
+          console.log(
+            'Erro ao carregar vídeo:',
+            JSON.stringify(error, null, 2),
+          );
+        }}
+        onPlaybackStateChanged={event => {
+          console.log('Status:', event);
+        }}
+      />
+
+      <Pressable
+        accessibilityRole="button"
+        accessibilityLabel={paused ? 'Reproduzir vídeo' : 'Pausar vídeo'}
+        onPress={togglePlayback}
+        style={styles.touchArea}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  info: {
+    position: 'absolute',
+    zIndex: 99,
+    left: 8,
+    padding: 8,
+  },
+  name: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    marginBottom: 4,
+    textShadowColor: 'rgba(0,0,0,0.60)',
+    textShadowOffset: { width: -1, height: 1.4 },
+    textShadowRadius: 8,
+  },
+  description: {
+    color: '#FFF',
+    marginRight: 34,
+    textShadowColor: 'rgba(0,0,0,0.20)',
+    textShadowOffset: { width: -1, height: 1.4 },
+    textShadowRadius: 8,
+  },
+  actions: {
+    position: 'absolute',
+    zIndex: 99,
+    right: 10,
+    bottom: Platform.OS === 'android' ? 120 : 170,
+    gap: 8,
+  },
+  actionText: {
+    textAlign: 'center',
+    fontSize: 13,
+    color: '#FFF',
+    fontWeight: 'bold',
+    textShadowColor: 'rgba(0,0,0,0.20)',
+    textShadowOffset: { width: -1, height: 1.4 },
+    textShadowRadius: 8,
+  },
+  touchArea: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  },
+});
--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,83 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  StatusBar,
+  Platform,
+  FlatList,
+  Dimensions,
+} from 'react-native';
+import { FeedItem } from '../../components/FeedItem';
+import { useState } from 'react';
+
+const feedItems = [
+  {
+    id: '1',
+    url: 'https://i.imgur.com/Cnz1CPK.mp4',
+    name: '@sujeitoprogramador',
+    description: 'Criando o ShotDev do Zero com RN.',
+    likes: 2102,
+    comments: 145,
+  },
+  {
+    id: '2',
+    url: 'https://i.imgur.com/E0tK2bY.mp4',
+    name: '@henriquesilva',
+    description:
+      'Fala turma, estou aprendendo React Native com sujeito programador.',
+    likes: 890,
+    comments: 89,
+  },
+  {
+    id: '3',
+    url: 'https://i.imgur.com/mPFvFyX.mp4',
+    name: '@sujeitoprogramador',
+    description: 'Aprendendo a trabalhar com Drag and Drop no React Native.',
+    likes: 112,
+    comments: 13,
+  },
+];
+
+const { height: heightScreen } = Dimensions.get('screen');
 
 export function Home() {
+  const [showItem, setShowItem] = useState(null);
+
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <View style={styles.labels}>
+        <TouchableOpacity>
+          <Text style={[styles.labelText, { color: '#DDD' }]}>Seguindo</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={[styles.labelText, { color: '#FFF' }]}>Para você</Text>
+          <View style={styles.indicator}></View>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={feedItems}
+        renderItem={({ item }) => (
+          <FeedItem data={item} currentVisibleItem={showItem} />
+        )}
+        keyExtractor={item => item.id}
+        showsVerticalScrollIndicator={false}
+        onViewableItemsChanged={({ viewableItems }) => {
+          if (viewableItems.length > 0) {
+            setShowItem(feedItems[viewableItems[0].index ?? 0]);
+          }
+        }}
+        snapToAlignment="center"
+        snapToInterval={heightScreen}
+        scrollEventThrottle={20}
+        decelerationRate={'fast'}
+        viewabilityConfig={{
+          waitForInteraction: false,
+          viewAreaCoveragePercentThreshold: 100,
+        }}
+      />
     </View>
   );
 }
@@ -11,7 +85,31 @@ export function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#000',
+  },
+  labels: {
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 8,
+    position: 'absolute',
+    top: 6,
+    left: 0,
+    right: 0,
+    marginTop:
+      Platform.OS === 'android' ? (StatusBar.currentHeight || 0) + 10 : 55,
+    zIndex: 99,
+  },
+  labelText: {
+    fontWeight: '500',
+    fontSize: 16,
+    marginBottom: 2,
+  },
+  indicator: {
+    width: 32,
+    height: 2,
+    alignSelf: 'center',
+    marginTop: 4,
+    backgroundColor: '#FFF',
   },
 });
